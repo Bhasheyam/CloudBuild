2b051be5237f0ea2dc7f60c64954a989adcf452d
Bhasheyam Krishnan <bhasheyam.krishnan@sap.com>
2021-06-17 15:29:19 -0700

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     """ Return a friendly HTTP greeting. """
     who = request.args.get("who", "World")
-    return f"Hello {who}!\n"
+    return f"Hello test {who}!\n"
 
 
 if __name__ == "__main__":
